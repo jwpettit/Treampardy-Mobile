afbe80386ea28ef65bf76b06cba927443a35a3cc
jwpettit <jwpettit@me.com>
2023-08-16 15:16:26 -0700

    Initial Commit

---
+++ Treampardy Mobile/Treampardy_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Treampardy_MobileApp.swift
+//  Treampardy Mobile
+//
+//  Created by Jeffrey Pettit on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Treampardy_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
