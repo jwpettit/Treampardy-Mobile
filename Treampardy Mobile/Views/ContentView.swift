79259200922f2ba6a2751e722d53e59f6142c013
jwpettit <jwpettit@me.com>
2023-08-17 10:33:15 -0700

    Started laying out home screen

---
+++ Treampardy Mobile/Views/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Treampardy Mobile
+//
+//  Created by Jeffrey Pettit on 8/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 40) {
+                VStack(spacing: 20) {
+                    Text("This is Treampardy!")
+                    
+                }
+                
+                NavigationLink {
+                    
+                } label: {
+                    
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
